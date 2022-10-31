510fd2332772e1d5fe56a442e6e97870a4a68c53
Sydney <tunes23@mail.wlu.edu>
2022-10-31 15:16:17 -0400

    looked at zipcodes and deleted stuff no needed

--- server.r
+++ server.r
@@ -17,51 +17,7 @@ function(input, output, session) {
       addTiles() %>%
       setView(lng = -93.85, lat = 37.45, zoom = 4)
   })
-  
-  # Don't need to return set of zips in bounds right now
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
- # zipsInBounds <- reactive({
-  #  if (is.null(input$map_bounds))
-   #   return(d_outcomes[FALSE,])
-  #  bounds <- input$map_bounds
-   # latRng <- range(bounds$north, bounds$south)
-  #  lngRng <- range(bounds$east, bounds$west)
-    
-  #  subset(zipdata,
-  #        latitude >= latRng[1] & latitude <= latRng[2] &
-  #          longitude >= lngRng[1] & longitude <= lngRng[2])
-#  })
-  
-  ## Put our plots (Chloropleths?)
-  
-  # Precalculate the breaks we'll need for the two histograms
- # centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
-  
- # output$histCentile <- renderPlot({
-    # If no zipcodes are in view, don't plot
-  #  if (nrow(zipsInBounds()) == 0)
-   #   return(NULL)
-    
-  #  hist(zipsInBounds()$centile,
-   #      breaks = centileBreaks,
-    #     main = "SuperZIP score (visible zips)",
-     #    xlab = "Percentile",
-      #   xlim = range(allzips$centile),
-       #  col = '#00DD00',
-        # border = 'white')
-  # })
-  
- # output$scatterCollegeIncome <- renderPlot({
-    # If no zipcodes are in view, don't plot
-  #  if (nrow(zipsInBounds()) == 0)
-  #    return(NULL)
-    
-  #  print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  # })
-  
-  # This observer is responsible for maintaining the circles and legend,
-  # according to the variables the user has chosen to map to color and size.
+
   
   # changing the color and size of the circles on top of the map
   # (that show the prevalence of the health outcomes)
@@ -69,18 +25,13 @@ function(input, output, session) {
     colorBy <- input$color
     sizeBy <- input$sizes
     
-    if (colorBy == "Year") {
+   
       colorData <- d_outcomes[[colorBy]]
       pal <- colorBin("viridis", colorData, 7, pretty = FALSE)
-    }
     
-    if (sizeBy == "MeasureId") {
-      # Radius is treated specially in the "MeasureId" case because
-      # the values are categorical instead of continuous.
-      radius <- ifelse(d_outcome$Data_Value >= (100 - input$threshold), 30000, 3000)
-    } else {
+    
       radius <- d_outcome[[sizeBy]] / max(d_outcome[[sizeBy]]) * 30000
-    }
+    
     
     # adds circles as markers on top of the existing map
     leafletProxy("map", data = d_outcome) %>%
@@ -165,18 +116,5 @@ function(input, output, session) {
     })
   })
   
-  output$ziptable <- DT::renderDataTable({
-    df <- cleantable %>%
-      filter(
-        Score >= input$minScore,
-        Score <= input$maxScore,
-        is.null(input$states) | State %in% input$states,
-        is.null(input$cities) | City %in% input$cities,
-        is.null(input$zipcodes) | Zipcode %in% input$zipcodes
-      ) %>%
-      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
-    action <- DT::dataTableAjax(session, df, outputId = "ziptable")
-    
-    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
-  })
+  
 }
